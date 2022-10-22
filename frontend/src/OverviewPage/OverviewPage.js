f8a3cdb6e882adb4bf1725ca2b4006d64773330b
mksah <mohit11sahoo@gmail.com>
2022-10-22 19:40:48 -0400

    Overview changes ;)

--- frontend/src/OverviewPage/OverviewPage.js
+++ frontend/src/OverviewPage/OverviewPage.js
@@ -1,74 +1,102 @@
 import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { PieChart, Pie, Tooltip } from "recharts"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { ResponsiveContainer, PieChart, Pie, Tooltip } from "recharts"
 import { auth } from "../firebase";
 import PlaidLink from "../PlaidLink/PlaidLink";
 
+
 const OverviewPage = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [user, loading] = useAuthState(auth);
-  const isPlaidNeeded = true;
+  // useEffect(() => {
+  //   auth.signOut();
+  // }, []);
+  const isPlaidNeeded = false;
   if (isPlaidNeeded) {
     return <PlaidLink />;
   }
+  // eslint-disable-next-line no-unused-vars
+  const [user, loading] = useAuthState(auth)
   return (
-    <div className="bg-[#F7F8FC]">
-      <PieChart width={730} height={250}>
-        <Pie
-          data={[
-            { name: "Utilities", value: 30 },
-            { name: "Financials", value: 70 },
-            { name: "Energy", value: 70 },
-            { name: "Consumer Staples", value: 70 },
-            { name: "Technology", value: 70 },
-          ]}
-          dataKey="value"
-          nameKey="name"
-          cx="50%"
-          cy="50%"
-          outerRadius={50}
-          fill="#274161"
-          label={(entry) => entry.name}
-          style={{ fontSize: "12px" }}
-        />
-        <Tooltip contentStyle={{ padding: "4px" }} />
-      </PieChart>
-      <div className="relative fixed py-5 px-4 flex justify-between items-center bg-white w-full">
+    <div className="bg-[#F7F8FC] w-[100%] overflow-y-scroll">
+      <div className="relative fixed py-5 px-4 flex justify-between items-center bg-white">
         <h2 className="font-semibold text-lg">Portfolio</h2>
-        <div>
-          <p className="font-roboto text-black">{user.email}</p>
+          <div>
+          <p
+            className="font-roboto text-black"
+          >
+            {user.email}
+          </p>
         </div>
       </div>
-      <div className="items-center">
-        <div className="flex justify-between text-justify overflow-scroll">
-          <div className="relative flex justify-between w-[334px] h-[95px] bg-darkBlue ml-60">
-            <p className="text-white font-semibold text-[20px] px-[25px] pt-[20px]">
-              Amount <br /> Invested
-            </p>
-            <p className="text-white font-bold pr-[15px] pt-[20px] text-[40px]">
-              $12,192
-            </p>
+      <div className="items-center mt-[15px] overflow-visible">
+        <div className="flex justify-between text-justify mx-[8%]">
+          <div className="relative flex justify-between w-[334px] h-[95px] bg-darkBlue">
+              <p className="text-white font-semibold text-[20px] px-[25px] pt-[20px]">Amount <br /> Invested</p>
+              <p className="text-white font-bold pr-[15px] pt-[20px] text-[40px]">$12,192</p>
           </div>
-          <div className="relative flex justify-between w-[334px] h-[95px] bg-white mx-20">
-            <p className="text-darkBlue font-semibold text-[20px] px-[25px] pt-[20px]">
-              Companies <br /> Invested
-            </p>
-            <p className="text-darkBlue font-bold pr-[15px] pt-[20px] text-[40px]">
-              88
-            </p>
+          <div className="relative flex justify-between w-[334px] h-[95px] bg-white  mx-[4%]">
+              <p className="text-darkBlue font-semibold text-[20px] pl-[25px] pr-[100px] pt-[20px]">Companies <br /> Invested</p>
+              <p className="text-darkBlue font-bold pr-[15px] pt-[20px] text-[40px]">88</p>
           </div>
-          <div className="relative flex justify-between w-[334px] h-[95px] bg-white mr-60">
-            <p className="text-darkBlue font-semibold text-[20px] px-[25px] pt-[20px]">
-              Sectors <br /> Invested
-            </p>
-            <p className="text-darkBlue font-bold pr-[15px] pt-[20px] text-[40px]">
-              5
-            </p>
+          <div className="relative flex justify-between w-[334px] h-[95px] bg-white">
+              <p className="text-darkBlue font-semibold text-[20px] pl-[25px] pr-[100px] pt-[20px]">Sectors <br /> Invested</p>
+              <p className="text-darkBlue font-bold pr-[15px] pt-[20px] text-[40px]">5</p>
           </div>
+        </div>
+      </div>
+      <div className="flex justify-between mx-[8%]">
+        <div className="relative bg-white mt-[15px] text-[20px] pt-[10px] w-[49%]">
+          <p className="text-black font-semibold ml-[5%] pb-[25px]">Sector Breakdown</p>
+          <ResponsiveContainer aspect={2}>
+            <PieChart width={300} height={300}>
+              <Pie
+                data={[
+                  { name: "Utilities", value: 30 },
+                  { name: "Financials", value: 70 },
+                  { name: "Energy", value: 70 },
+                  { name: "Consumer Staples", value: 70 },
+                  { name: "Technology", value: 70 },
+                ]}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                // outerRadius={50}
+                fill="#274161"
+                label={(entry) => entry.name}
+                style={{ fontSize: "12px" }}
+              />
+              <Tooltip contentStyle={{ padding: "4px" }} />
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="bg-white mt-[15px] text-[20px] pt-[10px] w-[49%]">
+          <p className="text-black font-semibold ml-[5%] pb-[25px]">Sector Details</p>
+          <ResponsiveContainer aspect={2}>
+            <PieChart width={300} height={300}>
+              <Pie
+                data={[
+                  { name: "Utilities", value: 30 },
+                  { name: "Financials", value: 70 },
+                  { name: "Energy", value: 70 },
+                  { name: "Consumer Staples", value: 70 },
+                  { name: "Technology", value: 70 },
+                ]}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                // outerRadius={50}
+                fill="#274161"
+                label={(entry) => entry.name}
+                style={{ fontSize: "12px" }}
+              />
+              <Tooltip contentStyle={{ padding: "4px" }} />
+            </PieChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
-  );
+  )
 };
 
 export default OverviewPage;
