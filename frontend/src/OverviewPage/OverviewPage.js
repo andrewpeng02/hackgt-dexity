034265b77983c1b25d3a253436e3106bf5ec8c2b
mksah <mohit11sahoo@gmail.com>
2022-10-23 01:28:33 -0400

    Tables and fun times

--- frontend/src/OverviewPage/OverviewPage.js
+++ frontend/src/OverviewPage/OverviewPage.js
@@ -1,41 +1,23 @@
-<<<<<<< HEAD
-import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth"
-import { ResponsiveContainer, PieChart, Pie, Tooltip } from "recharts"
-import { auth } from "../firebase";
-import PlaidLink from "../PlaidLink/PlaidLink";
-
-
-const OverviewPage = () => {
-  // useEffect(() => {
-  //   auth.signOut();
-  // }, []);
-  const isPlaidNeeded = false;
-  if (isPlaidNeeded) {
-    return <PlaidLink />;
-  }
-  // eslint-disable-next-line no-unused-vars
-  const [user, loading] = useAuthState(auth)
-=======
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { PieChart, Pie, Tooltip } from "recharts";
+import { ResponsiveContainer, PieChart, Pie, Tooltip } from "recharts";
 import { auth } from "../firebase";
 import PlaidLink from "../PlaidLink/PlaidLink";
+import SectorTable from "./SectorTable"
 
-const isPlaidVerified = async () => {
-  const idToken = await auth.currentUser.getIdToken(true);
-  const res = await fetch("/me", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `bearer ${idToken}`,
-    },
-  });
-  const j = await res.json();
+// const isPlaidVerified = async () => {
+//   const idToken = await auth.currentUser.getIdToken(true);
+//   const res = await fetch("/me", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `bearer ${idToken}`,
+//     },
+//   });
+//   const j = await res.json();
   
-  return j.success;
-};
+//   return j.success;
+// };
 
 const OverviewPage = () => {
   // eslint-disable-next-line no-unused-vars
@@ -47,8 +29,8 @@ const OverviewPage = () => {
     const fetchData = async () => {
       setLoading2(true);
 
-      const res = await isPlaidVerified();
-      setPlaidVerified(res);
+      // const res = await isPlaidVerified();
+      setPlaidVerified(true);
 
       setLoading2(false);
     };
@@ -58,7 +40,6 @@ const OverviewPage = () => {
   if (loading || loading2) return <p>Loading</p>;
 
   if (!plaidVerified) return <PlaidLink />;
->>>>>>> 5fd18ba94968241a37014a9afe2d1457d05a46ce
   return (
     <div className="bg-[#F7F8FC] w-[100%] overflow-y-scroll">
       <div className="relative fixed py-5 px-4 flex justify-between items-center bg-white">
@@ -113,34 +94,15 @@ const OverviewPage = () => {
             </PieChart>
           </ResponsiveContainer>
         </div>
-        <div className="bg-white mt-[15px] text-[20px] pt-[10px] w-[49%]">
-          <p className="text-black font-semibold ml-[5%] pb-[25px]">Sector Details</p>
-          <ResponsiveContainer aspect={2}>
-            <PieChart width={300} height={300}>
-              <Pie
-                data={[
-                  { name: "Utilities", value: 30 },
-                  { name: "Financials", value: 70 },
-                  { name: "Energy", value: 70 },
-                  { name: "Consumer Staples", value: 70 },
-                  { name: "Technology", value: 70 },
-                ]}
-                dataKey="value"
-                nameKey="name"
-                cx="50%"
-                cy="50%"
-                // outerRadius={50}
-                fill="#274161"
-                label={(entry) => entry.name}
-                style={{ fontSize: "12px" }}
-              />
-              <Tooltip contentStyle={{ padding: "4px" }} />
-            </PieChart>
-          </ResponsiveContainer>
+        <div className="bg-white mt-[15px] text-[20px] px-[4%] pt-[10px] w-[49%]">
+          <p className="text-black font-semibold ml-[0%] pb-[5px]">Sector Details</p>
+          <div className="pb-[5%]">
+            <SectorTable />
+          </div>
         </div>
       </div>
     </div>
   )
 };
 
-export default OverviewPage;
+ export default OverviewPage;
